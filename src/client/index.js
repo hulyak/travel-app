86207fc0fea03c7f2f9e5c8a973d54a2ee63f4bf
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-17 21:15:06 -0700

    remove typos

--- src/client/index.js
+++ src/client/index.js
@@ -5,11 +5,5 @@ console.log(checkForUrl);
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/form.scss';
-import './styles/trip.scss';
-
-import resort1 from './media/resort1.jpg';
-import resort2 from './media/resort2.jpg';
-import resort3 from './media/resort3.jpg';
-import boats from './media/boats.webp';
 
 export { handleSubmit };
--- src/client/js/handleSubmit.js
+++
@@ -1,4 +0,0 @@
-// const GEONAMES_URL = 'http://api.geonames.org/searchJSON?q=';
-// const GEONAMES_USERNAME = '&maxRows=1&username=hulya';
-// const PIXABAY_API_KEY = '16060501-e2d3132e99ce2be48e2344f5f';
-// const PIXABAY_URL = `https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=`;
