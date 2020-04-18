524c5daf1e69875e52616d5fb3f041ff72ce1c96
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-17 17:58:40 -0700

    change page design

--- src/client/index.js
+++ src/client/index.js
@@ -1,20 +1,19 @@
-import {
-    handleSubmit
-} from './js/formHandler';
-
+import { handleSubmit } from './js/formHandler';
 
 console.log(checkForUrl);
 //Import the sass files into index.js
-import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/header.scss'
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
 
 import resort1 from './media/resort1.jpg';
 import resort2 from './media/resort2.jpg';
 import resort3 from './media/resort3.jpg';
+import boats from './media/boats.jpg';
+
 export {
-    handleSubmit,
-    // checkForUrl
-}
+	handleSubmit
+	// checkForUrl
+};
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-const submit = document.querySelector('.btn');
+const submit = document.querySelector('#button');
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
