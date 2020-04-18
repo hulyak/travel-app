5d70cc77ac0308baccef7987a37b4f39c6b99f00
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-17 18:13:51 -0700

    add styling

--- src/client/index.js
+++ src/client/index.js
@@ -4,16 +4,12 @@ console.log(checkForUrl);
 //Import the sass files into index.js
 import './styles/resets.scss';
 import './styles/base.scss';
-import './styles/footer.scss';
 import './styles/form.scss';
-import './styles/header.scss';
+import './styles/trip.scss';
 
 import resort1 from './media/resort1.jpg';
 import resort2 from './media/resort2.jpg';
 import resort3 from './media/resort3.jpg';
-import boats from './media/boats.jpg';
+import boats from './media/boats.webp';
 
-export {
-	handleSubmit
-	// checkForUrl
-};
+export { handleSubmit };
