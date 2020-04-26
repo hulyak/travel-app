f1e2a9cbe767f33ab1f24a2cca2f0d0ef74d6bd5
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-26 10:11:28 -0700

    change style hover  effect

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -44,6 +44,6 @@ module.exports = {
             filename: "./index.html",
         }),
         new MiniCssExtractPlugin(),
-        new WorkboxPlugin.GenerateSW()
+
     ]
 }
