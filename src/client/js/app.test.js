b490cef3e695d11e42f67665bdfe87aa8695bc87
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-26 12:30:26 -0700

    solved async functionality in test

--- src/client/js/app.test.js
+++ src/client/js/app.test.js
@@ -9,34 +9,73 @@ const {
     onCreate
 } = require('./formvalidator.js');
 
-test('onCreate should be a function', () => {
-    expect(typeof onCreate).toBe('function');
+describe('onCreate() function', () => {
+    test("should be defined", async () => {
+        expect(onCreate).toBeDefined();
+    });
+    test('should be a function', async () => {
+        expect(typeof onCreate).toBe('function');
+    });
+});
+describe('callGeoNameApi() function', () => {
+    test('shoud be a defined ', async () => {
+        expect(callGeoNameAPI).toBeDefined();
+    });
+    test('should be a function', async () => {
+        expect(typeof callGeoNameAPI).toBe('function');
+    });
 });
 
-test(' allGeoNameAPI shoud be a function', () => {
-    expect(typeof callGeoNameAPI).toBe('function');
+describe('getWeatherData() function', () => {
+    test('shoud be a defined ', async () => {
+        expect(getWeatherData).toBeDefined();
+    });
+    test('getWeatherData shoud be a function', () => {
+        expect(typeof getWeatherData).toBe('function');
+    });
 });
 
-test('getWeatherData shoud be a function', () => {
-    expect(typeof getWeatherData).toBe('function');
+describe('getPixabayImages() function', () => {
+    test('shoud be a defined ', async () => {
+        expect(getPixabayImages).toBeDefined();
+    });
+    test('getPixabayImages shoud be a function', () => {
+        expect(typeof getPixabayImages).toBe('function');
+    });
 });
 
-test('getPixabayImages shoud be a function', () => {
-    expect(typeof getPixabayImages).toBe('function');
+describe('updateUI() function', () => {
+    test('shoud be a defined', async () => {
+        expect(updateUI).toBeDefined();
+    });
+    test('updateUI shoud be a function', () => {
+        expect(typeof updateUI).toBe('function');
+    });
 });
 
-test('updateUI shoud be a function', () => {
-    expect(typeof updateUI).toBe('function');
+describe('fetchLatLang() function', () => {
+    test('shoud be a defined ', async () => {
+        expect(fetchLatLang).toBeDefined();
+    });
+    test('fetchLatLang should be a function', () => {
+        expect(typeof fetchLatLang).toBe('function');
+    });
 });
 
-test('fetchLatLang should be a function', () => {
-    expect(typeof fetchLatLang).toBe('function');
+describe('postDataToServer() function', () => {
+    test('shoud be a defined ', async () => {
+        expect(postDataToServer).toBeDefined();
+    });
+    test('postDataToServer should be a function', () => {
+        expect(typeof postDataToServer).toBe('function');
+    });
 });
 
-test('postDataToServer should be a function', () => {
-    expect(typeof postDataToServer).toBe('function');
-});
-
-test('getDataFromServer should be a function', () => {
-    expect(typeof getDataFromServer).toBe('function');
+describe('getDataFromServer() function', () => {
+    test('shoud be a defined ', async () => {
+        expect(getDataFromServer).toBeDefined();
+    });
+    test('getDataFromServer should be a function', () => {
+        expect(typeof getDataFromServer).toBe('function');
+    });
 });
